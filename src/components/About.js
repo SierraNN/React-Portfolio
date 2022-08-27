3ec3d8224313d846d1eab71143ece3884c3c25b9
Sierra Nutt <sierra.nutt@outlook.com>
2022-08-27 09:16:24 -0600

    integrated bulma instead of tailwind

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,13 @@ import React from "react";
 export default function About() {
   return (
     <div>
-
+      <p>A highly motivated, results-driven full stack developer with solid experience
+      in data analysis and database management. A solid background in HTML, CSS,
+      Bulma, Tailwind, Node, Express, JavaScript, SQL, MongoDB, and React. My
+      ability to solve complex problems, along with my proven leadership qualities
+      have allowed me to build strong relationships throughout the enterprise that
+      allow for the development of strategic solutions for every challenge I face.
+      </p>
     </div>
   );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,10 @@ import React from "react";
 export default function Project() {
   return (
     <div>
+      <a href=""><img src="\public\assets\img1.jpeg"></img></a>
+      <a href=""><img src=""></img></a>
+      <a href=""><img src=""></img></a>
+      <a href=""><img src=""></img></a>
     </div>
   );
 }
