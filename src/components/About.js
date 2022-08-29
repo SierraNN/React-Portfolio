6e9178aef3ca5a8136c302d46a7e5250d9a05859
Sierra Nutt <sierra.nutt@outlook.com>
2022-08-29 09:07:33 -0600

    changes

--- src/components/About.js
+++ src/components/About.js
@@ -9,8 +9,7 @@ export default function About() {
       in data analysis and database management. A solid background in HTML, CSS,
       Bulma, Tailwind, Node, Express, JavaScript, SQL, MongoDB, and React. My
       ability to solve complex problems, along with my proven leadership qualities
-      have allowed me to build strong relationships throughout the enterprise that
-      allow for the development of strategic solutions for every challenge I face.
+      have allowed me to develop strategic solutions for every challenge I face.
       </p>
     </div>
   );
